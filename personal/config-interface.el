0816635d1ea34e7599520d063b3a5fe79488bd2c
Senkowo <94131088+senkowo@users.noreply.github.com>
2024-06-20 21:23:27 -0400

    added packages marginalia, embark, and embark-consult in config-interface.el

--- personal/config-interface.el
+++ personal/config-interface.el
@@ -1,3 +1,24 @@
 ;;; interface stuff
 
+;; fuzzy searching
 (global-set-key (kbd "C-s") 'consult-line)
+
+;; add descriptions and annotations to search results
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+;; act upon a selection item
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings))) ;; alternative for `describe-bindings'
+
+;; integration between embark and consult
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
